df6bae7482908c46a2a3f3490a6b59093db981f1
Angie Dahana <a.apontea@uniandes.edu.co>
2025-10-30 08:56:00 -0500

    prim

---
+++ DataStructures/Priority_queue/priority_queue.py
@@ -0,0 +1,11 @@
+from DataStructures.Priority_queue import pq_entry as pqe
+
+def default_compare_higher_value(father_node, child_node):
+    if pqe.get_priority(father_node) >= pqe.get_priority(child_node):
+        return True
+    return False
+
+def default_compare_lower_value(father_node, child_node):
+    if pqe.get_priority(father_node) <= pqe.get_priority(child_node):
+        return True
+    return False
