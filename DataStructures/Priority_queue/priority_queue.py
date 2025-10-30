c9704a59833e75662ab6fdafbe4929d9ba545493
Angie Dahana <a.apontea@uniandes.edu.co>
2025-10-30 09:13:15 -0500

    fun

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -9,3 +9,18 @@ def default_compare_lower_value(father_node, child_node):
     if pqe.get_priority(father_node) <= pqe.get_priority(child_node):
         return True
     return False
+
+def new_heap(is_min_pq):
+    cmp=default_compare_lower_value()
+    if is_min_pq is False:
+        cmp=default_compare_higher_value()
+          
+    queue={
+     'elements': {
+         'elements': [],
+         'size': 1
+     },
+         'size': 0,
+    'cmp_function': cmp
+    }
+    return queue
